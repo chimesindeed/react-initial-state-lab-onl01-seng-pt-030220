a795a95505167476d82ed3b63ef636598c503554
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-11-19 23:53:51 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -3,11 +3,14 @@ import React from './react'
 
 class ImageSlider extends React.Component {
   constructor(props){super(props)
-    this.state 
+    this.state = {
+      currentSlideIndex: 0
+    }
   }
   
   render = () => {
     return (
+      `I am on slide ${this.state.currentSlideIndex}`
       )
   }
   
